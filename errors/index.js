fec0d63fbf26a4ad21c5f26082eef1bdcd8947c7
David Street <david@exclamationlabs.com>
2016-08-31 14:08:33 -0400

    Set auth error name

--- errors/index.js
+++ errors/index.js
@@ -1,4 +1,9 @@
 'use strict'
 
-class AuthError extends Error {}
+class AuthError extends Error {
+	constructor(...args) {
+		super(...args)
+		this.name = 'Authentication Error'
+	}
+}
 exports.AuthError = AuthError
